40d31ba4ff1160b9476569d7ffa7047e2f837e9f
asx-dev <asxdev18@gmail.com>
2025-02-21 09:45:24 +0000

    Added Stack Data Structure

--- algorithms.js
+++ algorithms.js
@@ -93,6 +93,42 @@ class LinkedList {
 }
 
 // Stacks
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(value) {
+    this.stack.push(value);
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.stack.pop();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.stack[this.stack.length - 1];
+  }
+
+  isEmpty() {
+    return this.stack.length === 0;
+  }
+
+  size() {
+    return this.stack.length;
+  }
+
+  clear() {
+    this.stack = [];
+  }
+}
+
 // Queues
 // Hash Tables
 // Binary Tree
